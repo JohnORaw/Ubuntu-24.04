389d375573ce39d95fb5a0f7ec13e7f06d53500f
John ORaw <49677015+JohnORaw@users.noreply.github.com>
2025-03-22 10:44:33 +0000

    Create first.sh

---
+++ Initial/first.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# By: JOR
+# Date: 23Jan25
+# Function: First tasks, all servers
+# Script: first.sh
+
+# Updates
+sudo apt update
+sudo apt-upgrade -y
+
+# For minimum installs
+sudo apt install dnsutils -y
+sudo apt install iputils-ping -y
+sudo apt install nano -y
+sudo apt get openssh-server -y
