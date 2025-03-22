2c607da4ab963799a5d673e168e63974e8c1e046
John ORaw <49677015+JohnORaw@users.noreply.github.com>
2025-03-22 10:53:35 +0000

    Create first.sh

---
+++ 1. Installation/first.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# By: JOR
+# Date: 22MAR25
+# Function: Create local users with sudo, all servers
+# Script: localusers.sh
+
+create_user() {
+    username=$1
+    password=$2
+
+    # Create the user with the specified username
+    sudo useradd -m -s /bin/bash $username
+    # Set the user's password
+    echo "$username:$password" | sudo chpasswd
+    # Give the user sudo privilege 
+    sudo usermod -aG sudo $username
+    # Provide feedback
+    echo "User '$username' created with password '$password'"
+}
+
+# Create users
+create_user user1 password1
+create_user user2 password2
