2a8a91306b24b479cb20e5de4525ec48ef0ff343
John ORaw <49677015+JohnORaw@users.noreply.github.com>
2025-03-24 20:19:34 +0000

    Update first.sh

--- 1. Installation/first.sh
+++ 1. Installation/first.sh
@@ -2,23 +2,12 @@
 
 # By: JOR
 # Date: 22MAR25
-# Function: Create local users with sudo, all servers
-# Script: localusers.sh
+# Function: What to do first on all servers
+# Script: first.sh
 
-create_user() {
-    username=$1
-    password=$2
-
-    # Create the user with the specified username
-    sudo useradd -m -s /bin/bash $username
-    # Set the user's password
-    echo "$username:$password" | sudo chpasswd
-    # Give the user sudo privilege 
-    sudo usermod -aG sudo $username
-    # Provide feedback
-    echo "User '$username' created with password '$password'"
-}
-
-# Create users
-create_user user1 password1
-create_user user2 password2
+sudo apt update
+sudo apt-upgrade -y
+sudo apt install dnsutils -y
+sudo apt install iputils-ping -y
+sudo apt install nano -y
+sudo apt get openssh-server -y
